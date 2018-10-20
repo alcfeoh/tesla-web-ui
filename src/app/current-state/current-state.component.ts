873d65c9236f44c201b17f1507d83bcdf58ba549
Alain Chautard <alain@interstate21.com>
2018-10-19 21:12:41 -0700

    Start of a state component

--- src/app/current-state/current-state.component.ts
+++ src/app/current-state/current-state.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TeslaDataService} from '../tesla-data/tesla-data.service';
+import {DriveState, VehicleInfo} from '../tesla-data/responses';
 
 @Component({
   selector: 'app-current-state',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentStateComponent implements OnInit {
 
-  constructor() { }
+  state: VehicleInfo;
+
+
+  constructor(private service: TeslaDataService) {
+    // TODO Fix this shortcut. Here I'm assuming every account has one car only.
+    service.getVehicles().subscribe(state => this.state = state[0]);
+  }
 
   ngOnInit() {
   }
